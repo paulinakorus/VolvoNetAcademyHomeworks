5d0358546c46c8df8b36e4dd1491c9f6fac52ecd
Paulina Korus <paukor2606@gmail.com>
2023-12-03 19:40:03 +0100

    making repository

---
+++ Homework1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static double Addition(double x, double y)
+        {
+            return x + y;
+        }
+
+        static double Subtration(double x, double y)
+        {
+            return x - y;
+        } 
+
+
+    }
+}
