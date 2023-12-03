2d3703895d6ebcd9ec0fcc83aaeb4e034f06f6f8
Paulina Korus <paukor2606@gmail.com>
2023-12-03 20:08:46 +0100

    Making operations methods

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
 
         static double Addition(double x, double y)
@@ -17,6 +17,30 @@
             return x - y;
         } 
 
+        static double Multiplication(double x, double y)
+        {
+            return x * (double)y;
+        }
+
+        static double Division(double x, double y)
+        {
+            return x / (double)y;
+        }
+
+        static double Exponentiation (double x, int y)
+        {
+            return Math.Pow(x, y);
+        }
+
+        static long Factorial (int x)
+        {
+            if (x == 0)
+                return 1;
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        } 
 
     }
 }
