35a6f74a5784abba60457e65857ca9e225ea0f25
Paulina Korus <paukor2606@gmail.com>
2023-12-03 20:52:56 +0100

    Adding menu and switch the operation symbol

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,32 +4,60 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Homework 1");
+            Console.WriteLine("Calculating numbers");
+
+            partOne();
         }
 
-        static double Addition(double x, double y)
-        {
-            return x + y;
+        static void partOne() {
+            bool continuing = true;
+            while (continuing)
+            {
+                Console.WriteLine("\nPlease enter the numbers");
+                Console.Write("\tx: ");
+                var x = Console.ReadLine();
+                Console.Write("\ty: ");
+                var y = Console.ReadLine();
+
+                Console.WriteLine("\nPlease enter the symbol of the operation");
+                Console.WriteLine("\t+\t-\taddition");
+                Console.WriteLine("\t-\t-\tsubtration");
+                Console.WriteLine("\t*\t-\tmultiplication");
+                Console.WriteLine("\t/\t-\tdivision");
+                Console.WriteLine("\t^\t-\texponentiation");
+                Console.WriteLine("\t!\t-\tfactorial");
+
+                Console.Write("\nthe operation: ");
+                var operation = Console.ReadLine();
+                string result;
+                TheOperation(x, y, operation, out result);
+            }
         }
 
-        static double Subtration(double x, double y)
+        static double Addition(string x, string y)
         {
-            return x - y;
+            return Convert.ToDouble(x) + Convert.ToDouble(y);
+        }
+
+        static double Subtration(string x, string y)
+        {
+            return Convert.ToDouble(x) - Convert.ToDouble(y);
         } 
 
-        static double Multiplication(double x, double y)
+        static double Multiplication(string x, string y)
         {
-            return x * (double)y;
+            return Convert.ToDouble(x) * Convert.ToDouble(y);
         }
 
-        static double Division(double x, double y)
+        static double Division(string x, string y)
         {
-            return x / (double)y;
+            return Convert.ToDouble(x) / Convert.ToDouble(y);
         }
 
-        static double Exponentiation (double x, int y)
+        static double Exponentiation(string x, string y)
         {
-            return Math.Pow(x, y);
+            return Math.Pow(Convert.ToDouble(x), Convert.ToInt32(y));
         }
 
         static long Factorial (int x)
@@ -38,9 +66,44 @@
                 return 1;
             else
             {
-                return x * Factorial(x - 1);
+                return x * Factorial((x - 1));
             }
         } 
 
+        static void TheOperation (string x, string y, string operation, out string result)
+        {
+            switch (operation)
+            {
+                case "+":
+                    result = Addition(x, y).ToString();
+                    Console.WriteLine(result);
+                    break;
+                case "-":
+                    result = Subtration(x, y).ToString();
+                    Console.WriteLine(result);
+                    break;
+                case "*":
+                    result = Multiplication(x, y).ToString();
+                    Console.WriteLine(result);
+                    break;
+                case "/":
+                    result = Division(x, y).ToString();
+                    Console.WriteLine(result);
+                    break;
+                case "^":
+                    result = Exponentiation(x, y).ToString();
+                    Console.WriteLine(result);
+                    break;
+                case "!":
+                    result = Factorial(int.Parse(x)).ToString();
+                    Console.WriteLine(result);
+                    break;
+                default: 
+                    result = null;
+                    Console.WriteLine("The symbol is incorrect");
+                    Console.WriteLine("Try again\n");
+                    break;
+            }
+        }
     }
 }
