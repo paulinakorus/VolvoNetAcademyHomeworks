c7803924a8d726723f8eae43ab59f16d9386f21b
Paulina Korus <paukor2606@gmail.com>
2024-02-11 20:58:10 +0100

    Extraxting ZIP file in no parallel version

---
+++ Homework3/Program.cs
@@ -0,0 +1,14 @@
+﻿using Homework3.service;
+
+namespace Homework3
+{
+    internal class Program
+    {
+        private const string FILE_TO_ZIP = @"C:\Users\pauko\Desktop\Studia\Kursy\Volvo NET Academy\Homework\Homework3\data\100 books.zip.zip";
+        static void Main(string[] args)
+        {
+            ZipFileOpener zipOpener = new ZipFileOpener();
+            zipOpener.OpenZip(FILE_TO_ZIP);
+        }
+    }
+}
---
+++ Homework3/service/ZipFileOpener.cs
@@ -0,0 +1,52 @@
+﻿using System.IO.Compression;
+
+namespace Homework3.service;
+
+public class ZipFileOpener
+{
+    string fullPathOfNewFolder = @"C:\Users\pauko\Desktop\Studia\Kursy\Volvo NET Academy\Homework\Homework3\data";
+
+    public void OpenZip(string filePath)
+    {
+        IfEndsWithDirectorySeparator(fullPathOfNewFolder);
+
+        using (ZipArchive archive = ZipFile.OpenRead(filePath))
+        {
+            foreach (ZipArchiveEntry entry in archive.Entries)
+            {
+                //string nameOfTXT = GettingNameOfTXT(entry);
+                GettingNameOfTXT(entry);
+                if (entry.Name.EndsWith(".txt"))
+                {
+                    string destinationPath = Path.GetFullPath(Path.Combine(fullPathOfNewFolder, entry.FullName));
+                    if (destinationPath.StartsWith(fullPathOfNewFolder) && !File.Exists(destinationPath))
+                        entry.ExtractToFile(destinationPath);
+                }
+            }
+        }
+    }
+
+    private string IfEndsWithDirectorySeparator(string path)
+    {
+        if (!path.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            return path += Path.DirectorySeparatorChar;
+        return path;
+    }
+
+    private void GettingNameOfTXT(ZipArchiveEntry entry)
+    {
+        string nameOfFile = entry.FullName;
+        
+        if (nameOfFile.Contains(@"/"))
+        {
+            int index = nameOfFile.IndexOf(@"/");
+            string nameOfDirectory = nameOfFile.Substring(0, index);
+            //string nameOfTXT = nameOfFile.Substring(index+1);
+            string fullPath = Path.Combine(fullPathOfNewFolder, nameOfDirectory);
+            if(!Directory.Exists(fullPath))
+                Directory.CreateDirectory(fullPath);
+            //return nameOfTXT;
+        }
+        //return "";
+    }
+}
