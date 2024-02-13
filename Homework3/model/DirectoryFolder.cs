37bde84c5a63d5a249b948706bf02a3fda7cf83f
Paulina Korus <paukor2606@gmail.com>
2024-02-13 09:36:39 +0100

    Getting book text and removing useless lines

--- Homework3/model/DirectoryFolder.cs
+++ Homework3/model/DirectoryFolder.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Homework3.model;
 
@@ -25,27 +26,66 @@ public class DirectoryFolder
             });
             await Task.WhenAll(tasks);*/
 
-            var task = WorkingWithFile()
+            string file_Path = @"C:\Users\pauko\Desktop\Studia\Kursy\Volvo NET Academy\Homework\Homework3\data\100 books\pg10007.txt";
+            var task = WorkingWithFile(file_Path);
+            await task;
         }
     }
 
     private async Task WorkingWithFile(string filePath)
     {
-        var text = await File.ReadAllTextAsync(filePath);
+        //var text = await File.ReadAllTextAsync(filePath);
+        var text = await GetDataFromFileAsync(filePath);
         Console.WriteLine(text);
+
+        //string[] sentences = Regex.Split(text, @"(?<=[\.!\?])\s+");
     }
 
-    /*public async Task<string> GetDataFromFileAsync(string path)
+    private bool IfRemoveLine(string text, string[] wordsToRemove)
+    {
+        foreach (string word in wordsToRemove)
+        {
+            if (text.ToLower().Contains(word))
+                return true;
+        }
+        return false;
+    }
+
+    public async Task<string> GetDataFromFileAsync(string path)
     {
         var list = new List<string>();
+        bool foundStart = false;
+        string startPattern = "START";
+        string endPattern = "END";
+        string seperationPattern = "***";
+        string[] wordsToRemove = {"chapter"};
+
         using (var reader = new StreamReader(File.OpenRead(path)))
         {
             while (!reader.EndOfStream)
             {
                 var line = await reader.ReadLineAsync();
-                list.Add(line);
+                if (!foundStart)
+                {
+                    if (line.Contains(startPattern) && line.Contains(seperationPattern))
+                    {
+                        foundStart = true;
+                    }
+                }
+                else if ((line.Contains(endPattern) && line.Contains(seperationPattern)))
+                {
+                    break;
+                }
+                else if (IfRemoveLine(line, wordsToRemove))
+                {
+                    continue;
+                }
+                else
+                {
+                    list.Add(line);
+                }
             }
             return list.Aggregate((current, line) => current += line);
         }
-    }*/
+    }
 }
